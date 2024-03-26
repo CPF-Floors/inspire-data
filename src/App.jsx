40c188a7e82ed7120e9a5a3ba2170120618ca04d
Alfredo <offsound2020@gmail.com>
2024-03-26 10:26:28 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css"
+
+
+function App() {
+
+
+
+
+
+  return (
+    <>
+      <div className="inspire-data-container">
+        <img
+          src="https://cpffloors.com/wp-content/uploads/2024/03/Group-82.svg"
+          alt=""
+        />
+        <p>To access all the information about the IBE campaign click the button bellow</p>
+        <button>Download File</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
